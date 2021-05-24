9cfffeb18e4c244455d60a759d89068283b1de4d
Pulsar <62395480+Woodnet@users.noreply.github.com>
2021-05-24 15:11:56 +0200

    Create Winkel_zwischen_Vektoren_berechnen-3D.py

---
+++ Winkel_zwischen_Vektoren_berechnen-3D.py
@@ -0,0 +1,15 @@
+import os,math
+
+os.system("cls") #Windows
+r = (-4,1,5)
+s = (6,-2,9)
+
+skalar_produkt = (r[0] * s[0] + r[1] * s[1] + r[2] * s[2])
+betrag_r = ((r[0])**2 + (r[1])**2 + (r[2])**2)
+betrag_s = ((s[0])**2 + (s[1])**2 + (r[2])**2)
+quotient = ((skalar_produkt)/(math.sqrt(betrag_r) * math.sqrt(betrag_s)))
+alpha = math.degrees((math.acos(quotient)))
+print("Skalar-Produkt= %s"%(skalar_produkt))
+print("|r|= %s"%(betrag_r))
+print("|s|= %s"%(betrag_s))
+print("Winkel zischen den Vektoren= %s°"%(alpha))
